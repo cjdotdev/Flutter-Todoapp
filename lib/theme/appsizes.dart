83953260a06b9cb7e5cb8b233fe240a887314791
alpha1207vj <gide749@gmail.com>
2026-07-01 12:59:06 +0100

    New commbit2

--- lib/theme/appsizes.dart
+++ lib/theme/appsizes.dart
@@ -1,7 +1,7 @@
 class AppSizes {
   AppSizes._();
 
-  static const double xs = 4;
+  static const double xs = 8;
   static const double sm = 8;
   static const double md = 16;
   static const double lg = 24;
